ef5837fb3eb4f4ec30ff24df28598ee62a8f04ed
suibe_sjl <youngquant_sjl@outlook.com>
2025-01-02 23:15:35 +0800

    add models

---
+++ model/mtl/ple.py
@@ -0,0 +1,188 @@
+'''
+Reference:
+    [1]Jiaqi Ma et al. Modeling task relationships in multi-task learning with multi-gate mixture-of-experts. In Proceedings of the 24th ACM SIGKDD
+    International Conference on Knowledge Discovery & Data Mining, pages 1930–1939, 2018.
+Reference:
+    https://github.com/busesese/MultiTaskModel
+'''
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class PLE(nn.Module):
+    """
+    MMOE for CTCVR problem
+    """
+
+    def __init__(self, user_feature_dict, item_feature_dict, emb_dim=128, 
+                task=['ctr', 's5', 's10', 's30', 'follow'], shared_expert=1,
+                hidden_dim=[128, 64], tower_dim = [64, 32], dropouts=0.5,
+                output_size=1, expert_activation=F.relu, device=None):
+        """
+        MMOE model input parameters
+        :param user_feature_dict: user feature dict include: {feature_name: (feature_unique_num, feature_index)}
+        :param item_feature_dict: item feature dict include: {feature_name: (feature_unique_num, feature_index)}
+        :param emb_dim: int embedding dimension
+        :param n_expert: int number of experts in mmoe
+        :param task_hidden_dim: list task layer hidden dimension
+        :param hidden_dim: list task tower hidden dimension
+        :param dropouts: task dnn drop out probability
+        :param output_size: int task output size
+        :param expert_activation: activation function like 'relu' or 'sigmoid'
+        :param task: list of task name
+        """
+        super(PLE, self).__init__()
+        # check input parameters
+        if user_feature_dict is None or item_feature_dict is None:
+            raise Exception("input parameter user_feature_dict and item_feature_dict must be not None")
+        if isinstance(user_feature_dict, dict) is False or isinstance(item_feature_dict, dict) is False:
+            raise Exception("input parameter user_feature_dict and item_feature_dict must be dict")
+
+        self.user_feature_dict = user_feature_dict
+        self.item_feature_dict = item_feature_dict
+        self.expert_activation = expert_activation
+        self.num_task = len(task)
+        self.shared_expert = shared_expert
+        self.task = task
+        # n_expert = len(task)
+
+        if device:
+            self.device = device
+
+        # embedding初始化
+        user_cate_feature_nums, item_cate_feature_nums = 0, 0
+        for user_cate, num in self.user_feature_dict.items():
+            if num[0] > 1:
+                user_cate_feature_nums += 1
+                setattr(self, user_cate, nn.Embedding(num[0], emb_dim))
+        for item_cate, num in self.item_feature_dict.items():
+            if num[0] > 1:
+                item_cate_feature_nums += 1
+                setattr(self, item_cate, nn.Embedding(num[0], emb_dim))
+
+        # user embedding + item embedding
+        hidden_size = emb_dim * (user_cate_feature_nums + item_cate_feature_nums) + \
+                      (len(self.user_feature_dict) - user_cate_feature_nums) + (
+                              len(self.item_feature_dict) - item_cate_feature_nums)
+
+        # shared-expert
+        # input_dim = embedding_dim
+        # output_dim = 64
+        for i in range(self.shared_expert):
+            setattr(self, 'share_{}_dnn'.format(i+1), nn.ModuleList())
+            share_hidden_dim = [hidden_size] + hidden_dim # embedding_dim -> 128 -> 64
+            for j in range(len(share_hidden_dim)-1):
+                # j= 0,1
+                getattr(self, 'share_{}_dnn'.format(i+1)).add_module('share_hidden_{}'.format(j),
+                                                                    nn.Linear(share_hidden_dim[j], share_hidden_dim[j+1]))
+                getattr(self, 'share_{}_dnn'.format(i+1)).add_module('share_batchnorm_{}'.format(j),
+                                                                    nn.BatchNorm1d(share_hidden_dim[j+1]))
+                getattr(self, 'share_{}_dnn'.format(i+1)).add_module('share_dropout_{}'.format(j),
+                                                                    nn.Dropout(dropouts))
+
+        # task-expert
+        # input_dim = embedding_dim
+        # output_dim = 64
+        for task_name in task:
+            setattr(self, '{}_expert_dnn'.format(task_name), nn.ModuleList())
+            task_hidden_dim = [hidden_size] + hidden_dim #embedding_dim,128,64
+            for j in range(len(task_hidden_dim)-1):
+                getattr(self, '{}_expert_dnn'.format(task_name)).add_module('{}_expert_hidden_{}'.format(task_name, j),
+                                                                            nn.Linear(task_hidden_dim[j], task_hidden_dim[j+1]))
+                getattr(self, '{}_expert_dnn'.format(task_name)).add_module('{}_expert_batchnorm_{}'.format(task_name, j),
+                                                                            nn.BatchNorm1d(task_hidden_dim[j+1]))
+                getattr(self, '{}_expert_dnn'.format(task_name)).add_module('{}_expert_dropout_{}'.format(task_name, j),
+                                                                            nn.Dropout(dropouts))
+
+            
+        # gates
+        self.gates = [torch.nn.Parameter(torch.rand(hidden_size, shared_expert+1), requires_grad=True) for _ in
+                      range(num_task)] # gate 形状是[-1,2,64]
+        for gate in self.gates:
+            gate.data.normal_(0, 1)
+        self.gates_bias = [torch.nn.Parameter(torch.rand(shared_expert+1), requires_grad=True) for _ in range(num_task)]
+
+
+        # tower
+        for task_name in task:
+            setattr(self, '{}_tower_dnn'.format(task_name), nn.ModuleList())
+            tower_hidden_dim = tower_dim #embedding_dim,128,64
+            for j in range(len(tower_hidden_dim)-1):
+                getattr(self, '{}_tower_dnn'.format(task_name)).add_module('{}_tower_hidden_{}'.format(task_name, j),
+                                                                            nn.Linear(tower_hidden_dim[j], tower_hidden_dim[j+1]))
+                getattr(self, '{}_tower_dnn'.format(task_name)).add_module('{}_tower_batchnorm_{}'.format(task_name, j),
+                                                                            nn.BatchNorm1d(tower_hidden_dim[j+1]))
+                getattr(self, '{}_tower_dnn'.format(task_name)).add_module('{}_tower_dropout_{}'.format(task_name, j),
+                                                                            nn.Dropout(dropouts))
+            getattr(self,'{}_tower_dnn'.format(task_name)).add_module('{}_tower_laset_layer'.format(task_name),
+                                                                            nn.Linear(tower_hidden_dim[-1], output_size))
+
+
+    def forward(self, x):
+        assert x.size()[1] == len(self.item_feature_dict) + len(self.user_feature_dict)
+        # embedding
+        user_embed_list, item_embed_list = list(), list()
+        for user_feature, num in self.user_feature_dict.items():
+            if num[0] > 1:
+                user_embed_list.append(getattr(self, user_feature)(x[:, num[1]].long()))
+            else:
+                user_embed_list.append(x[:, num[1]].unsqueeze(1))
+        for item_feature, num in self.item_feature_dict.items():
+            if num[0] > 1:
+                item_embed_list.append(getattr(self, item_feature)(x[:, num[1]].long()))
+            else:
+                item_embed_list.append(x[:, num[1]].unsqueeze(1))
+
+        # embedding 融合
+        user_embed = torch.cat(user_embed_list, axis=1)
+        item_embed = torch.cat(item_embed_list, axis=1)
+
+        # hidden layer
+        hidden = torch.cat([user_embed, item_embed], axis=1).float()  # batch * hidden_size
+
+        # shared-expert
+        share_experts = list()
+        for i in range(self.shared_expert):
+            share_expert = hidden
+            for share in getattr(self, 'share_{}_dnn'.format(i+1))
+                share_expert = share(share_expert)
+            share_experts.append(share_expert)
+
+        # task-expert
+        task_experts = list()
+        for task_name in self.task:
+            task_expert = hidden
+            for task_nn in getattr(self, '{}_expert_dnn'.format(task_name)):
+                task_expert = task_nn(task_expert)
+            task_experts.append(task_expert)
+
+        # gate
+        gates_out = list()
+        for idx, gate in enumerate(self.gates):
+            gate = gate.to(self.device)
+            gate_out = torch.einsum('ab, bc -> ac', hidden, gate)  # batch * num_experts
+            if self.gates_bias:
+                self.gates_bias[idx] = self.gates_bias[idx].to(self.device)
+                gate_out += self.gates_bias[idx]
+            gate_out = nn.Softmax(dim=-1)(gate_out)
+            gates_out.append(gate_out)
+
+        # 加权
+        all_cgc = list()
+        for i, task_name in enumerate(task):
+            combined_expert_outputs = torch.cat(share_experts[0]+task_experts[i], dim=-1)
+            task_out = torch.sum(gates_out[i].unsqueeze(-1)*combined_expert_outputs, dim=0)
+            all_cgc.append(task_out)
+
+        # task tower
+        task_outputs = list()
+        for i, task_name in enumerate(task):
+            x = all_cgc[i]
+            for mod in getattr(self, '{}_tower_dnn'.format(task_name)):
+                x = mod(x)
+            task_outputs.append(x)
+        return task_outputs
+
+
+
